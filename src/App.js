201ca600287ea6d5a25e14d4c92b4d6eb59ab8da
webdevops <shukrullozaylobidinov@gmail.com>
2022-09-06 15:13:16 +0500

    crypto price tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './priceTracker/main.css'
+import Coin from './priceTracker/coin'
+import Logo from './priceTracker/logo'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <Coin />
     </div>
   );
 }
---
+++ src/priceTracker/logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../images/logo.png'
+import {Col, Container, Row} from "reactstrap";
+
+function Logo(props) {
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col md="12" className='text-center mt-5'>
+                        <img src={logo} alt="Logo"/>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
+}
+
+export default Logo;
